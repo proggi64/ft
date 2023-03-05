0b3cb71be16ec7df14c430d92f1241f17164f771
Karsten Beck <kb.github@webkasi.de>
2023-03-05 13:02:28 +0100

    Updates

--- index.php
+++ index.php
@@ -12,4 +12,14 @@ $parts = new ftPart($db);
 $parts->Test();
 
 printf("Success... %s\n", $db->host_info);
+
+$beimAnlegen = 'password';
+$hashInDatenbank = password_hash($beimAnlegen, PASSWORD_DEFAULT);
+$beimPruefen = 'passwordx';
+$test = password_verify($beimPruefen, $hashInDatenbank);
+if ($test)
+    echo '<p>Passwort OK</p>';
+else
+    echo '<p>Falsches Passwort</p>';
+
 ?>
--- lib/ftKit.php
+++ lib/ftKit.php
@@ -3,6 +3,26 @@
 class ftKit extends ftTableBase {
     private const TABLENAME = 'Kit';
 
+    public function Create(array $data)
+    {
+        [$partNo, $description, $price, $comment, $firstYear, $lastYear] = $data;
+        //string $sql = "CALL ";
+    }
+
+    public function Read(int $id)
+    {
+        
+    }
+
+    public function Update(int $id, array $data)
+    {
+        [$partNo, $description, $price, $comment, $firstYear, $lastYear] = $data;
+    }
+
+    public function Delete(int $id)
+    {
+
+    }
 }
 
 ?>
